6ad1cca6dd368e7b5782c6248a8c36addfe5c02f
Mazlai <micky31200@gmail.com>
2023-03-13 18:46:27 +0100

    troisième commit avec code

--- src/MarsRover.php
+++ src/MarsRover.php
@@ -3,9 +3,9 @@ namespace MRover;
 
 class MarsRover {
 
-    public int $x;
-    public int $y;
-    public String $pos;
+    private int $x;
+    private int $y;
+    private String $pos;
 
     public function __construct() {
         $this->x = 0;
@@ -13,12 +13,118 @@ class MarsRover {
         $this->pos = 'N';
     }
 
-    public function move() {
+    public function move($sequence) {
+        //Découpe la séquence en une série de caractères individuels
+        $directions = str_split($sequence);
 
+        foreach($directions as $direction) {
+            switch($direction) {
+                case "":
+                    return "";
+
+                case 'R':
+                    if($this->pos === 'N') {
+                        $this->pos = 'E';
+                    } else {
+                        if ($this->pos === 'E') {
+                            $this->pos = 'S';
+                        } else {
+                            if ($this->pos === 'S') {
+                                $this->pos = 'W';
+                            } else {
+                                $this->pos = 'N';
+                            }
+                        }
+                    }
+                    break;
+
+                case 'L':
+                    if($this->pos === 'N') {
+                        $this->pos = 'W';
+                    } else {
+                        if ($this->pos === 'W') {
+                            $this->pos = 'S';
+                        } else {
+                            if ($this->pos === 'S') {
+                                $this->pos = 'E';
+                            } else {
+                                $this->pos = 'N';
+                            }
+                        }
+                    }
+                    break;
+
+                case 'M':
+                    switch($this->pos) {
+                        case 'N':
+                            $this->y++;
+                            if($this->y == 10) {
+                                $this->y = 0;
+                            }
+                            break;
+                        case 'S':
+                            $this->y--;
+                            if($this->y == -1) {
+                                $this->y = 9;
+                            }
+                            break;
+                        case 'E':
+                            $this->x++;
+                            if($this->x == 10) {
+                                $this->x = 0;
+                            }
+                            break;
+                        case 'W':
+                            $this->x--;
+                            if($this->x == -1) {
+                                $this->x = 9;
+                            }
+                            break;
+                    }
+                    break;
+                
+                default:
+                    echo "Votre position doit être valable";
+                    break;
+
+            }
+        }
+
+        $count = count($directions);
+        echo($count);
+        if($count > 4) {
+            $mod = $count % 4;
+
+            switch($this->pos) {
+                case 'R':
+                    if($mod === 1) {
+                        $this->pos = 'E';
+                    } else if ($mod === 2) {
+                        $this->pos = "S";
+                    } else if($mod === 3) {
+                        $this->pos = "W";
+                    } else {
+                        $this->pos = "N";
+                    }
+                    break;
+
+                case 'L':
+                    if($mod === 1) {
+                        $this->pos = 'W';
+                    } else if ($mod === 2) {
+                        $this->pos = "S";
+                    } else if($mod === 3) {
+                        $this->pos = "E";
+                    } else {
+                        $this->pos = "N";
+                    }
+                    break;
+            }
+        }
     }
 
     public function position() {
-        $position = $this->x.','.$this->y.','.$this->pos;
+        $position = strval($this->x).", ".strval($this->y).", ".strval($this->pos);
         return $position;
     }
 
--- test/testMarsRover.php
+++ test/testMarsRover.php
@@ -11,11 +11,232 @@ class testMarsRover extends TestCase {
         $marsRover = new MarsRover();
 
         // WHEN
-        $position = $marsRover->move("");
+        $position = $marsRover->position();
 
         // THEN
         $this->assertEquals("0, 0, N", $position);
     }
 
+    /** @test */
+    function notMoving() {
+        // GIVEN
+        $marsRover = new MarsRover();
+
+        // WHEN
+        $marsRover->move("");
+        $position = $marsRover->position();
+
+        // THEN
+        $this->assertEquals("0, 0, N", $position);
+    }
+
+    /** @test */
+    function moveRightOneTime() {
+        // GIVEN
+        $marsRover = new MarsRover();
+
+        // WHEN
+        $marsRover->move("R");
+        $position = $marsRover->position();
+
+        // THEN
+        $this->assertEquals("0, 0, E", $position);
+    }
+
+    /** @test */
+    function moveRightTwoTimes() {
+        // GIVEN
+        $marsRover = new MarsRover();
+
+        // WHEN
+        
+        $marsRover->move("RR");
+        $position = $marsRover->position();
+
+        // THEN
+        $this->assertEquals("0, 0, S", $position);
+    }
+
+    /** @test */
+    function moveRightThreeTimes() {
+        // GIVEN
+        $marsRover = new MarsRover();
+
+        // WHEN
+        $marsRover->move("RRR");
+        $position = $marsRover->position();
+
+        // THEN
+        $this->assertEquals("0, 0, W", $position);
+    }
+
+    /** @test */
+    function moveRightFourTimes() {
+        // GIVEN
+        $marsRover = new MarsRover();
+
+        // WHEN
+        $marsRover->move("RRRR");
+        $position = $marsRover->position();
+
+        // THEN
+        $this->assertEquals("0, 0, N", $position);
+    }
+
+    /** @test */
+    function moveRightFiveTimes() {
+        // GIVEN
+        $marsRover = new MarsRover();
+
+        // WHEN
+        $marsRover->move("RRRRR");
+        $position = $marsRover->position();
+
+        // THEN
+        $this->assertEquals("0, 0, E", $position);
+    }
+
+    /** @test */
+    function moveLeftOneTime() {
+        // GIVEN
+        $marsRover = new MarsRover();
+
+        // WHEN
+        $marsRover->move("L");
+        $position = $marsRover->position();
+
+        // THEN
+        $this->assertEquals("0, 0, W", $position);
+    }
+
+    /** @test */
+    function moveLeftTwoTimes() {
+        // GIVEN
+        $marsRover = new MarsRover();
+
+        // WHEN
+        $marsRover->move("LL");
+        $position = $marsRover->position();
+
+        // THEN
+        $this->assertEquals("0, 0, S", $position);
+    }
+
+    /** @test */
+    function moveLeftThreeTimes() {
+        // GIVEN
+        $marsRover = new MarsRover();
+
+        // WHEN
+        $marsRover->move("LLL");
+        $position = $marsRover->position();
+
+        // THEN
+        $this->assertEquals("0, 0, E", $position);
+    }
+
+    /** @test */
+    function moveLeftFourTimes() {
+        // GIVEN
+        $marsRover = new MarsRover();
+
+        // WHEN
+        $marsRover->move("LLLL");
+        $position = $marsRover->position();
+
+        // THEN
+        $this->assertEquals("0, 0, N", $position);
+    }
+
+    /** @test */
+    function moveLeftFiveTimes() {
+        // GIVEN
+        $marsRover = new MarsRover();
+
+        // WHEN
+        $marsRover->move("LLLLL");
+        $position = $marsRover->position();
+
+        // THEN
+        $this->assertEquals("0, 0, W", $position);
+    }
+
+    /** @test */
+    function moveForward() {
+        // GIVEN
+        $marsRover = new MarsRover();
+
+        // WHEN
+        $marsRover->move("M");
+        $position = $marsRover->position();
+
+        // THEN
+        $this->assertEquals("0, 1, N", $position);
+    }
+
+    /** @test */
+    function moveBackward() {
+        // GIVEN
+        $marsRover = new MarsRover();
+
+        // WHEN
+        $marsRover->move("RRM");
+        $position = $marsRover->position();
+
+        // THEN
+        $this->assertEquals("0, 9, S", $position);
+    }
+
+    /** @test */
+    function moveInAnotherWay() {
+        // GIVEN
+        $marsRover = new MarsRover();
+
+        // WHEN
+        $marsRover->move("ML");
+        $position = $marsRover->position();
+
+        // THEN
+        $this->assertEquals("0, 1, W", $position);
+    }
+
+    /** @test */
+    function moveMultipleTimes() {
+        // GIVEN
+        $marsRover = new MarsRover();
+
+        // WHEN
+        $marsRover->move("MMMMM");
+        $position = $marsRover->position();
+
+        // THEN
+        $this->assertEquals("0, 5, N", $position);
+    }
+
+    /** @test */
+    function moveMultipleTimesWithDifferentPositions() {
+        // GIVEN
+        $marsRover = new MarsRover();
+
+        // WHEN
+        $marsRover->move("MMRRRRRML");
+        $position = $marsRover->position();
+
+        // THEN
+        $this->assertEquals("1, 2, N", $position);
+    }
+
+    /** @test */
+    function moveMultipleTimesInASameDirection() {
+        // GIVEN
+        $marsRover = new MarsRover();
+
+        // WHEN
+        $marsRover->move("MMMMMMMMMM");
+        $position = $marsRover->position();
+
+        // THEN
+        $this->assertEquals("0, 0, N", $position);  
+    }
 
 }
